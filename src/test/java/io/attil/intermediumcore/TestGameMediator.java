3e42d6b671104c449fdb4efb2486ec0f0e611e33
Attilio <parrotchaffinch@gmail.com>
2016-08-07 09:39:34 +0200

    Dummy test, JUnit added as dep.

---
+++ src/test/java/io/attil/intermediumcore/TestGameMediator.java
@@ -0,0 +1,14 @@
+package io.attil.intermediumcore;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class TestGameMediator {
+
+	@Test
+	public void testMediatorCretion() {
+		assertFalse(true);
+	}
+	
+}
