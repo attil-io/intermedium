1d33159636bcc0bad0ca9b05f9ac802cd2dceb44
Attilio <parrotchaffinch@gmail.com>
2016-08-07 18:27:16 +0200

    PlayField checks for boundary collision.

--- src/main/java/io/attil/gameengine/PlayField.java
+++ src/main/java/io/attil/gameengine/PlayField.java
@@ -4,8 +4,13 @@ import io.attil.intermediumcore.Mediator;
 
 public class PlayField extends GameObject {
 
-	public PlayField(Mediator mediator) {
+	private int maxX;
+	private int maxY;
+	
+	public PlayField(Mediator mediator, int maxX, int maxY) {
 		super(mediator);
+		this.maxX = maxX;
+		this.maxY = maxY;
 	}
 
 	@Override
@@ -15,7 +20,18 @@ public class PlayField extends GameObject {
 
 	@Override
 	public void onMove(GameObject sender, int deltaX, int deltaY) {
-
+		if (sender.getPosX() + deltaX < 0) {
+			mediator.sendMessage(this, new CollisionMessage(this, sender));
+		}
+		if (sender.getPosY() + deltaY < 0) {
+			mediator.sendMessage(this, new CollisionMessage(this, sender));
+		}
+		if (sender.getPosX() + deltaX >= maxX) {
+			mediator.sendMessage(this, new CollisionMessage(this, sender));
+		}
+		if (sender.getPosY() + deltaY >= maxY) {
+			mediator.sendMessage(this, new CollisionMessage(this, sender));
+		}
 	}
 
 }
--- src/test/java/io/attil/gameengine/TestPlayField.java
+++ src/test/java/io/attil/gameengine/TestPlayField.java
@@ -1,6 +1,11 @@
 package io.attil.gameengine;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import io.attil.intermediumcore.Message;
+import io.attil.intermediumcore.Colleague;
 import io.attil.intermediumcore.Mediator;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,10 +26,13 @@ public class TestPlayField {
 	
 	@Mock
 	private CollisionMessage collisionMessage;
+
+	@Mock
+	private GameObject gameObject;
 	
 	@Before
 	public void setUp() {
-		playField = new PlayField(mediator);
+		playField = new PlayField(mediator, 8, 5);
 	}
 	
 	@Test
@@ -53,4 +61,43 @@ public class TestPlayField {
 		assertEquals(0, playField.getPosY());
 	}
 
+	@Test
+	public void testNoCollideIfInside() throws Exception {
+		when(gameObject.getPosX()).thenReturn(2);
+		when(gameObject.getPosY()).thenReturn(2);
+		playField.onMove(gameObject, -1, 0);
+		verify(mediator, times(0)).sendMessage(any(Colleague.class), any(CollisionMessage.class));
+	}
+	
+	@Test
+	public void testCollideIfMinXReached() throws Exception {
+		when(gameObject.getPosX()).thenReturn(0);
+		when(gameObject.getPosY()).thenReturn(0);
+		playField.onMove(gameObject, -1, 0);
+		verify(mediator, times(1)).sendMessage(eq(playField), any(CollisionMessage.class));
+	}
+
+	@Test
+	public void testCollideIfMinYReached() throws Exception {
+		when(gameObject.getPosX()).thenReturn(0);
+		when(gameObject.getPosY()).thenReturn(0);
+		playField.onMove(gameObject, 0, -1);
+		verify(mediator, times(1)).sendMessage(eq(playField), any(CollisionMessage.class));
+	}
+
+	@Test
+	public void testCollideIfMaxXReached() throws Exception {
+		when(gameObject.getPosX()).thenReturn(9);
+		when(gameObject.getPosY()).thenReturn(0);
+		playField.onMove(gameObject, 1, 0);
+		verify(mediator, times(1)).sendMessage(eq(playField), any(CollisionMessage.class));
+	}
+
+	@Test
+	public void testCollideIfMaxYReached() throws Exception {
+		when(gameObject.getPosX()).thenReturn(0);
+		when(gameObject.getPosY()).thenReturn(4);
+		playField.onMove(gameObject, 0, 1);
+		verify(mediator, times(1)).sendMessage(eq(playField), any(CollisionMessage.class));
+	}
 }
